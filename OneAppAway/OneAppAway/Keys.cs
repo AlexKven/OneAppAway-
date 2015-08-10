8f38ef58538549143d4a784b65f59f2fb61f68d6
AlexKven <mathandprogramming@gmail.com>
2015-08-09 20:45:29 -0700

    Project Recreation

---
+++ OneAppAway/OneAppAway/Keys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneAppAway
+{
+    internal static class Keys
+    {
+        public static string BingMapKey { get; } = "s5030Pi5RRfQpmN9G31Y~Av27qRni0nZtyuLdCWdmgw~AroE4QWDxcQzLO1BLE1Dsm9t_hi6TG9z-8Kxr6v5D01QZFP4BacIgGWRRnuYcRJM";
+        public static string ObaKey { get; } = "bb69ae35-224e-4003-9fdd-33998988b5b5";
+    }
+}
